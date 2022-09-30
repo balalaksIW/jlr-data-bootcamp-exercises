ec241e6da0b692d2ac5dfe6906e00119b82aa93c
Balaji Lakshmanan <102376624+balalaksIW@users.noreply.github.com>
2022-09-30 14:52:38 +0100

    Create app.py

---
+++ Week-3/Unit-Testing-1/app.py
@@ -0,0 +1,11 @@
+def price_updater(prices, increase_rate):
+    if increase_rate > 1 or increase_rate < 0:
+        raise ValueError
+    new_price_list = []
+    for price in prices:
+        if isinstance(price, (float, int)):
+            new_price = price + (price * increase_rate)
+            new_price_list.append(new_price)
+        else:
+            return "Incorrect Price Detected!"
+    return new_price_list
